0e93d1c22e30060da383241161612aff4b6d3e72
OmarNizam <eng.omarnizam@gmail.com>
2017-10-17 18:10:13 +0200

    Connecting the API and Database

--- routes.js
+++ routes.js
@@ -2,60 +2,89 @@
 
 var express = require('express')
 var router = express.Router()
-var Question = require('./models')
+var Question = require('./models').Question
+
+// Express allows us to trigger a handler whenever the URL parameter is present on any matchiong route.
+router.param("qID", function(req, res, next, id) { // param method take two parameters first our route as String and the another is call back method // id is the value from the route parameter
+  Question.findById(id, function(err, doc) {
+    if(err) return next(err)
+    if(!doc){
+      err = new Error('Not Found')
+      err.status = 404
+      next(err)
+    }
+    req.question = doc
+    next()
+  })
+})
+
+router.param("aID", function(req, res, next, id) {
+  req.answer = req.question.answers.id(id)  // the ID method take sub-document ID and returns that sub-document with that matching ID
+  if(!req.answer){
+    const err = new Error('Not Found')
+    err.status = 404
+    next(err)
+  }
+  next()
+})
+
 // GET /questions
 // route for questions collection
-router.get('/', (req, res) => {
-  res.json({
-    response: "You send me a GET request "
-  })
+router.get('/', (req, res, next) => {
+  Question.find({})                    // find all questions
+              .sort({ updatedAt: -1 }) // sort the result
+              .exec(function(err, questions) {   // this execute the query and called the callback function
+                if(err) return next(err)
+                res.json(questions)         // mongoose allows us to pass the document into json file
+              })
 })
 
 // POST /questions
 // router for creating questions
-router.post('/', (req, res) => {
-  res.json({
-    response: "Yoy send me a POST request",
-    body: req.body
+router.post('/', (req, res, next) => {
+  var question = new Question(req.body)
+  question.save(function(err, question) {
+    if(err) return next(err)
+    res.status(201)  // to indicate that the question saved successfully
+    res.json(question)
   })
 })
 
 // GET /questions:qID
 // route for a specific question
-router.get('/:qID', (req, res) => {
-  res.json({
-    response: "You send me a GET request for ID" + req.params.qID
+router.get('/:qID', (req, res, next) => {
+    res.json(req.question)
   })
-})
+
 
 // POST /questions:qID/answers
 // route to create answers for a specific question
-router.post('/:qID/answers', (req, res) => {
-  res.json({
-    response: "You send me a POST to /answers",
-    questionId: req.params.qID,
-    body: req.body
+router.post('/:qID/answers', (req, res, next) => {
+  req.question.answers.push(req.body)
+  req.question.save(function(err, question) {
+    if(err) return next(err)
+    res.status(201)  // to indicate that the question saved successfully
+    res.json(question)
   })
 })
 
 // PUT /questions/:qID/answers/:aID
 // Edit or update a specific answer
-router.put('/:qID/answers/:aID', (req, res) => {
-  res.json({
-    response: "You send me a PUT to /answers ",
-    questionId: req.params.qID,
-    answerId: req.params.aID,
-    body: req.body
+router.put('/:qID/answers/:aID', (req, res, next) => {
+  req.answer.update(req.body, function(err, result) {
+    if(err) return next(err)
+    res.json(result)
   })
 })
 
 // DELETE /questions/:qID/answers/:aID
 // delete a specific answer
-router.delete('/:qID/answers/:aID', (req, res) => {
-  res.json({
-    response: "You send me a DELETE answer to /answers",
-    questionId: req.params.qID,
-    answerId: req.params.aID
+router.delete('/:qID/answers/:aID', (req, res, next) => {
+  req.answer.remove(function(err) {
+    req.question.save(function(err, question) {
+      if(err) return next(err)
+      res.json(question)
+    })
   })
 })
 
@@ -68,14 +97,13 @@ router.post('/:qID/answers/:aID/vote-:dir', (req, res, next) => {
     err.status = 404
     next(err)
   } else {
+    req.vote = req.params.dir
     next()
   }
-}, (req, res) => {  // dir here is direction
-  res.json({
-    response: "You send me a POST request to /vote-" + req.params.dir,
-    questionId: req.params.qID,
-    answerId: req.params.aID,
-    vote: req.params.dir
+}, (req, res, next) => {  // dir here is direction
+  req.answer.vote(req.vote, function(err, question) {
+    if(err) return next(err)
+    res.json(question)
   })
 })
 
